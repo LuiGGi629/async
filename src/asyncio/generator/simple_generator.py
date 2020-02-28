bd57e90ca3d044e4569574319dff790eed562439
LuiGGi629 <wojtek.felix.zuber@gmail.com>
2020-02-28 06:25:26 +0100

    async and await with asyncio

---
+++ src/asyncio/generator/simple_generator.py
@@ -0,0 +1,32 @@
+from typing import List, Generator
+
+
+# def fib(n: int) -> List[int]:
+#     numbers = []
+#     current, nxt = 0, 1
+#     while len(numbers) < n:
+#         current, nxt = nxt, current + nxt
+#         numbers.append(current)
+#
+#     return numbers
+#
+#
+# print(fib(10))
+
+
+def fib() -> Generator[int, None, None]:
+    current, nxt = 0, 1
+    while True:
+        current, nxt = nxt, current + nxt
+        yield current
+
+
+result = fib()
+
+for n in result:
+    print(n, end=', ')
+    if n > 10000:
+        break
+
+print()
+print('Done')
