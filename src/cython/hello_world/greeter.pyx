0a24447d16fbb31cf7cbda2f8d0ec8f7b96518d8
LuiGGi629 <wojtek.felix.zuber@gmail.com>
2020-03-04 11:56:13 +0100

    cython

---
+++ src/cython/hello_world/greeter.pyx
@@ -0,0 +1,3 @@
+def greeter(name: str):
+    print(f"Elo {name}")
+    print(f"** Running from {__file__} **")
---
+++ src/cython/hello_world/program.py
@@ -0,0 +1,10 @@
+from greeter import greeter
+
+
+def main():
+    name = input("What is your name?")
+    greeter(name)
+
+
+if __name__ == '__main__':
+    main()
